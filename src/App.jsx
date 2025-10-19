4c9d08c581881dc1867b95f18c9f8b8516745768
Gitte <gittevandevenne@hotmail.com>
2025-10-19 18:40:20 +0300

    hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import './App.css'
 import Home from "./pages/Home.jsx";
 import CategoryPage from "./pages/CategoryPage.jsx";
 import RecipePage from "./pages/RecipePage.jsx";
 
-
 function App() {
     return (
-        <BrowserRouter basename="/recipes">
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/category/:categoryId" element={<CategoryPage/>}/>
                 <Route path="/recipe/:recipeId" element={<RecipePage/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
